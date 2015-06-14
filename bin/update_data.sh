d60f5152606903ae25e8f6eb76cb3a9b2f8c3107
Tangui Morlier <tangui@tangui.eu.org>
2015-06-15 00:08:01 +0200

    Convert from french CSV (; separator) to , separator

--- bin/update_data.sh
+++ bin/update_data.sh
@@ -4,8 +4,8 @@ cd $(dirname $0)/..
 
 . config/config.inc
 
-boobank -f csv list | sed 's/^[0-9]*EUR@//' > data/list.csv
-boobank history -f csv $BANKID | sed 's/^[0-9]*@//' > data/.history.csv
+boobank -f csv list | sed 's/^[0-9]*EUR@//' | sed 's/,/./g' | sed 's/;/,/g' > data/list.csv
+boobank history -f csv $BANKID | sed 's/^[0-9]*@//' | sed 's/,/./g' | sed 's/;/,/g' > data/.history.csv
 cat data/history.csv data/.history.csv | sort -ur > data/history.csv
 
 git add data/list.csv data/history.csv
