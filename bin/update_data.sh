542792199bb7bad32a19b1e4191a3bfa7b8b669f
Tangui Morlier <tangui@tangui.eu.org>
2015-07-11 14:05:19 +0200

    Correct non historisation bug

--- bin/update_data.sh
+++ bin/update_data.sh
@@ -6,7 +6,8 @@ cd $(dirname $0)/..
 
 boobank -f csv list | sed 's/^[0-9]*EUR@//' | sed 's/,/./g' | sed 's/;/,/g' > data/list.csv
 boobank history -f csv $BANKID | sed 's/^[0-9]*@//' | sed 's/,/./g' | sed 's/;/,/g' > data/.history.csv
-cat data/history.csv data/.history.csv | sort -ur > data/history.csv
+cat data/history.csv data/.history.csv | sort -ur > data/.history.csv.tmp
+mv data/.history.csv.tmp data/history.csv
 
 git add data/list.csv data/history.csv
 if ! test "$1" = "nocommit" && git commit -m "update bank situation" > /dev/null ; then
