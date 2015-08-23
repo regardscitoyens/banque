8646afa4be77e21272b5d3dd933673c0735cbb06
RouxRC <b.ooghe@gmail.com>
2015-08-23 22:55:44 +0200

    ensure daily autoupdate always get 20 lines back

--- bin/update_data.sh
+++ bin/update_data.sh
@@ -7,6 +7,7 @@ workon boobankRC
 
 source config.inc
 
+# Collect and format details on all accounts
 boobank -f csv list > data/list.csv.tmp 2> /tmp/boobank.list.log ||
  ( echo "ERROR collecting list of accounts" && cat /tmp/boobank.list.log && exit 1 )
 cat data/list.csv.tmp |
@@ -15,8 +16,9 @@ cat data/list.csv.tmp |
  csvcut -d ";" -c "id,label,balance,currency,coming,type" > data/list.csv
 rm -f data/list.csv.tmp
 
+# Collect and format recent history for each account
 for BANKID in $CREDITMUTUEL $PAYPAL; do
-  boobank history -f csv $BANKID > data/.history.${BANKID}.csv.tmp 2> /tmp/boobank.${BANKID}.history.log ||
+  boobank history -f csv $BANKID -n 20 > data/.history.${BANKID}.csv.tmp 2> /tmp/boobank.${BANKID}.history.log ||
    ( echo "ERROR collecting history for $BANKID" && cat /tmp/boobank.${BANKID}.history.log && exit 1 )
   cat data/.history.${BANKID}.csv.tmp |
    sed 's/^[0-9A-Z]*@//' |
@@ -27,9 +29,11 @@ for BANKID in $CREDITMUTUEL $PAYPAL; do
   rm -f data/.history.${BANKID}.csv.tmp
 done
 
+# Merge new entries into global history
 cat data/history.csv data/.history.*@*.csv | sort -ur > data/.history.csv.tmp
 mv data/.history.csv.tmp data/history.csv
 
+# Auto commit if not debugging
 if ! test "$1" = "nocommit"; then
   git add data/list.csv data/history.csv
   if git commit -m "update bank situation" > /dev/null ; then
