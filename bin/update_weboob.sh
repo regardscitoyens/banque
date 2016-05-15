0539cb13ec293117df1e35354c1a8c6024d3cb08
RCbot <regardscitoyens@gmail.com>
2016-05-15 19:18:27 +0200

    better weboob update script

--- bin/update_weboob.sh
+++ bin/update_weboob.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd $(dirname $0)/../weboob
-source ../config.inc || exit 1
-
+cd $(dirname $0)/..
+source config.inc || exit 1
+cd weboob
+git pull
 python setup.py install
 weboob-config update
