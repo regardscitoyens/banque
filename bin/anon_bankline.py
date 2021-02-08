35dd39531e740af3bf941b1bc1fb7030cec3f123
RouxRC <b.ooghe@gmail.com>
2021-02-08 22:53:59 +0100

    fix broken ano on some cozycloud lines

--- bin/anon_bankline.py
+++ bin/anon_bankline.py
@@ -9,7 +9,7 @@ re_ano = re.compile(ur'(Don( récurrent)? de )(\w)[^\s\.]+( +(\w)[^\s\.].*)?$', 
 sub_ano = lambda x: x.group(1) + (x.group(3) + "." + (x.group(5) or "") + ".").upper()
 ano = lambda x: re_ano.sub(sub_ano, x)
 #Virement Anonymisation VIR M. XXXX YYYY => VIR M. X.Y.
-re_ano2 = re.compile(ur'M\. ([A-Z])[A-Z]+ ([A-Z])[A-Z]+ ([0-9\- ]+) .*')
+re_ano2 = re.compile(ur'M\. ([A-Z])[A-Z]+ ([A-Z])[A-Z]+ ([0-9\- ]+)( .*|$)')
 sub_ano2 = lambda x: "M. "+x.group(1)+"."+x.group(2)+". "+x.group(3)
 ano2 = lambda x: re_ano2.sub(sub_ano2, x)
 
